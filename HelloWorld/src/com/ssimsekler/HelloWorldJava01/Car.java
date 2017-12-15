6fe87209f6ace6862e4c0ee80d542945e4978b7f
Serdar Simsekler <ssimsekler@yahoo.com>
2017-12-15 17:00:33 +0000

    test

---
+++ HelloWorld/src/com/ssimsekler/HelloWorldJava01/Car.java
@@ -0,0 +1,22 @@
+package com.ssimsekler.HelloWorldJava01;
+
+public class Car {
+
+	public String name;
+	
+
+	public Car(String name) {
+		super();
+		this.name = name;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ HelloWorld/src/com/ssimsekler/HelloWorldJava01/HelloWorld.java
@@ -0,0 +1,12 @@
+
+package com.ssimsekler.HelloWorldJava01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		Car porsche = new Car("My Porsche");
+
+	}
+
+}
